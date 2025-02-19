910dfd1edab3b9d09106e51bd13a115621bbae38
ThomasMLT <thomas.milliot04@gmail.com>
2025-02-19 19:47:29 +0100

    add spring connection in the factory

--- Assets/RobotFactory.cs
+++ Assets/RobotFactory.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class RobotFactory : MonoBehaviour
 {
     public GameObject robot;
-
     public GameObject body;
     public GameObject wheel;
     public GameObject springConnection;
@@ -11,11 +10,21 @@ public class RobotFactory : MonoBehaviour
 
     void BuildRobot() {
 
-        GameObject newRobot =Instantiate(robot, new Vector3(0, 0, 0), Quaternion.identity);
-        Instantiate(body, new Vector3(0, 0, 0), Quaternion.identity, newRobot.transform);
-        Instantiate(wheel, new Vector3(0,0.9f,0), Quaternion.identity, newRobot.transform);
-        Instantiate(springConnection, new Vector3(0,0.7f,0), Quaternion.identity, newRobot.transform);
+        GameObject newRobot = Instantiate(robot, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject Body = Instantiate(body, new Vector3(0, 0, 0), Quaternion.identity, newRobot.transform);
+        GameObject wheel0 = Instantiate(wheel, new Vector3(0,0.8f,0), Quaternion.identity, newRobot.transform);
+        GameObject springconnection0 = Instantiate(springConnection, new Vector3(0,0.6f,0), Quaternion.identity, newRobot.transform);
+        GameObject wheel1 = Instantiate(wheel, new Vector3(0,-0.8f,0), Quaternion.identity, newRobot.transform);
+        GameObject springconnection1 = Instantiate(springConnection, new Vector3(0,-0.6f,0), Quaternion.identity, newRobot.transform);
         
+        // initiate connections
+        SpringJoint2D[] springConnections0 = springconnection0.GetComponents<SpringJoint2D>();
+        SpringJoint2D[] springConnections1 = springconnection1.GetComponents<SpringJoint2D>();
+        springConnections0[0].connectedBody = Body.GetComponent<Rigidbody2D>();
+        springConnections0[1].connectedBody = wheel0.GetComponent<Rigidbody2D>();
+        springConnections1[0].connectedBody = Body.GetComponent<Rigidbody2D>();
+        springConnections1[1].connectedBody = wheel1.GetComponent<Rigidbody2D>();
+
 
     }
 
